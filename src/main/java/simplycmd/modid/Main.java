1c065d3c2a51f451b82d9364de35a0608ff04193
SimplyCmd <simplycmd00@gmail.com>
2021-12-12 13:14:56 -0700

    simply-ify

---
+++ src/main/java/simplycmd/modid/Main.java
@@ -0,0 +1,23 @@
+package simplycmd.modid;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main implements ModInitializer, ClientModInitializer {
+	public static final String MOD_ID = "modid";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info(MOD_ID + " activated :O");
+
+		ItemReg.register();
+        BlockReg.register();
+	}
+
+	@Override
+    public void onInitializeClient() {
+    }
+}
